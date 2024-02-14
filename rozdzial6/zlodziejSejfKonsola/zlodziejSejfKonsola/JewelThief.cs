d67752e4723ca173914b51c70f3093e7b30cd406
Bobsonn123 <jakubowski.marcel07@gmail.com>
2024-02-14 21:32:28 +0100

    kik

---
+++ rozdzial6/zlodziejSejfKonsola/zlodziejSejfKonsola/JewelThief.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zlodziejSejfKonsola
+{
+    internal class JewelThief : Locksmith
+    {
+        private string stolenJewels;
+        protected override void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine($"Kradnę kosztowności! Ukradłem: {stolenJewels}");
+        }
+    }
+}
---
+++ rozdzial6/zlodziejSejfKonsola/zlodziejSejfKonsola/SafeOwner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zlodziejSejfKonsola
+{
+    internal class SafeOwner
+    {
+        private string valuables = "";
+        public void RecieveContents(string safeContents)
+        {
+            valuables = safeContents;
+            Console.WriteLine($"Moje {valuables}! Dziękuje za zwrot!");
+        }
+    }
+}
