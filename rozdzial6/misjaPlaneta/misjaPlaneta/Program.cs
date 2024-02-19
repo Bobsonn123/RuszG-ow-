144e6fb16d96df065effc5d71c2d5e97c30f60bd
Bobsonn123 <jakubowski.marcel07@gmail.com>
2024-02-19 17:07:03 +0100

    f

---
+++ rozdzial6/misjaPlaneta/misjaPlaneta/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace misjaPlaneta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Venus().MissionInfo());
+            Console.WriteLine(new Mars().MissionInfo());
+        }
+    }
+}
--- rozdzial6/programOPtakach/programOPtakach/Bird.cs
+++ rozdzial6/programOPtakach/programOPtakach/Bird.cs
@@ -6,13 +6,9 @@ using System.Threading.Tasks;
 
 namespace programOPtakach
 {
-    internal class Bird
+    abstract class Bird
     {
         public static Random Randomizer = new Random();
-        public virtual Egg[] LayEggs(int numberOfEggs)
-        {
-            Console.Error.WriteLine("Metoda Bird.LayEggs nie powinna być wywołana");
-            return new Egg[0];
-        }
+        public abstract Egg[] LayEggs(int numberOfEggs).;
     }
 }
