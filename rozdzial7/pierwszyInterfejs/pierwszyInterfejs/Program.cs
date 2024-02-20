ad8e1fc0c2205b5d535c13a8cf8b95d6242fac93
Bobsonn123 <jakubowski.marcel07@gmail.com>
2024-02-20 23:03:10 +0100

    ddd

---
+++ rozdzial7/pierwszyInterfejs/pierwszyInterfejs/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace pierwszyInterfejs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 183, Name = "Jeremi" };
+            tallGuy.TalkAboutYourself();
+            Console.WriteLine($"Ten wysoki gość ma {tallGuy.FunnyThingIHave}.");
+            tallGuy.Honk();
+        }
+    }
+}
---
+++ rozdzial7/pierwszyInterfejs/pierwszyInterfejs/TallGuy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pierwszyInterfejs
+{
+    internal class TallGuy : IClown
+    {
+        public string Name;
+        public int Height;
+
+        public string FunnyThingIHave { get { return "wielkie buty"; } }
+
+        public void Honk()
+        {
+            Console.WriteLine("Bip, bip!");
+        }
+        public void TalkAboutYourself(){
+            Console.WriteLine($"Nazywam się {Name}. Mam {Height} cm wzrostu.");
+        }
+    }
+}
