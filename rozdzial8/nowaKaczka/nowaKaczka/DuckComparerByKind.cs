708ef8760d882d2d33ebfd624d4976d1ab83c630
Bobsonn123 <jakubowski.marcel07@gmail.com>
2024-02-27 22:26:42 +0100

    l

---
+++ rozdzial8/nowaKaczka/nowaKaczka/DuckComparerByKind.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nowaKaczka
+{
+    internal class DuckComparerByKind : IComparer<Duck>
+    {
+        public int Compare(Duck x, Duck y)
+        {
+            if (x.Kind < y.Kind)
+            {
+                return -1;
+            }
+            else if (x.Kind > y.Kind)
+            {
+                return 1;
+            }
+            else return 0;
+        }
+    }
+}
---
+++ rozdzial8/nowaKaczka/nowaKaczka/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace nowaKaczka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IComparer<Duck> sizeComparer = new DuckComparerByKind();
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck() {Kind = KindOfDuck.krzyżówka, Size = 43},
+                new Duck() {Kind = KindOfDuck.piżmowa, Size = 45},
+                new Duck() {Kind = KindOfDuck.nur, Size = 35},
+                new Duck() {Kind = KindOfDuck.piżmowa, Size = 27},
+                new Duck() {Kind = KindOfDuck.krzyżówka, Size = 35},
+                new Duck() {Kind = KindOfDuck.nur, Size = 33},
+            };
+            ducks.Sort(sizeComparer);
+            PrintDucks(ducks);
+        }
+
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach(Duck duck in ducks)
+            {
+                Console.WriteLine($"{duck.Kind}, {duck.Size} cm");
+            }    
+        }
+    }
+}
