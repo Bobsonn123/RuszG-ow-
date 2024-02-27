18bead510c6867855f7709c3eb2479264562fee2
Bobsonn123 <jakubowski.marcel07@gmail.com>
2024-02-27 22:59:00 +0100

    jhf

---
+++ rozdzial8/aplikacjaDoKart/aplikacjaDoKart/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplikacjaDoKart
+{
+    internal class Card
+    {
+        public Values Value {  get;  set; }
+        public Suits Suit {  get;  set; }
+        public Card(Values value, Suits suit) 
+        {
+            Value = value;
+            Suit = suit;
+        }
+
+        public string Name { get { return $"{Value} {Suit}"; } }
+    }
+}
---
+++ rozdzial8/aplikacjaDoKart/aplikacjaDoKart/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace aplikacjaDoKart
+{
+    internal class Program
+    {
+
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            List<Card> cards = new List<Card>();
+
+            Console.Write("Podaj liczbę kart: ");
+            string answer = Console.ReadLine();
+
+            if(int.TryParse(answer, out int times)) 
+            {
+                for(int i = 0; i < times; i++)
+                {
+                    cards.Add(RandomCard());
+                }
+            }
+            PrintCards(cards);
+
+            Console.WriteLine("\nsortowanie kart\n");
+
+            cards.Sort(new CardComparerByValue());
+
+            PrintCards(cards);
+        }
+        static Card RandomCard()
+        {
+            return new Card((Values)random.Next(1, 14), (Suits)random.Next(1, 4));
+        }
+
+        static void PrintCards(List<Card> cards)
+        {
+            foreach(Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
+        }
+    }
+}
