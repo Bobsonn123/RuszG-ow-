4dce650bb9974e0e55a14cb6664305e9c39772f1
Bobsonn123 <jakubowski.marcel07@gmail.com>
2024-02-28 21:28:18 +0100

    i

---
+++ rozdzial8/drwalNalesniki/drwalNalesniki/Lumberjack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drwalNalesniki
+{
+    internal class Lumberjack
+    {
+        public string Name { get; set; }
+
+        public Lumberjack (string name)
+        {
+            Name = name;
+        }
+
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+
+        public void TakeFlapjack(Flapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+
+        public void EatFlapjacks()
+        {
+            Console.WriteLine($"{Name} je naleśniki");
+            while( flapjackStack.Count > 0 )
+            {
+                Console.WriteLine($"{Name} je {flapjackStack.Pop().ToString().ToLower()} naleśnik.");
+            }
+        }
+    }
+}
---
+++ rozdzial8/drwalNalesniki/drwalNalesniki/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace drwalNalesniki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Queue<Lumberjack> lumberjacks = new Queue<Lumberjack>();  
+
+            Console.Write("Imię pierwszego drwala: ");
+            string name;
+
+            while ((name = Console.ReadLine()) != "")
+            {
+                Console.Write("Liczba naleśników: ");
+                string liczba = Console.ReadLine();
+
+                if (int.TryParse(liczba, out int nalesniki))
+                {
+                    Lumberjack lumberjack = new Lumberjack(name);
+
+                    for(int i = 0; i < nalesniki; i++) lumberjack.TakeFlapjack((Flapjack)random.Next(1, 4));
+
+                    lumberjacks.Enqueue(lumberjack);
+                }
+                Console.Write("Imię następnego drwala (pusty wiersz, aby zakończyć): ");
+            }
+
+            foreach (Lumberjack lumberjack in lumberjacks)
+            {
+                lumberjack.EatFlapjacks();
+            }
+        }
+    }
+}
